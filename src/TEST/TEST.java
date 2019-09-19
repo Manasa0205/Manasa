16f26516b4f2daecd39f632f1290e598bb552744
Manasa0205 <manasakesamreddy@gmail.com>
2019-09-19 14:01:59 +0530

    program

---
+++ src/TEST/TEST.java
@@ -0,0 +1,18 @@
+package TEST;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TEST
+{
+	@BeforeClass
+	public void brower() {
+		System.out.println("brower");
+
+	}
+	@Test
+	public void add() 
+	{
+		System.out.println("dhfhfh");
+	}
+}
